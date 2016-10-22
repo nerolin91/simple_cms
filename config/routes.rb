a33457ba6d2b40d11ef5ce77e30b645aec80261d
Nero Lin <nerolin91@gmail.com>
2016-10-22 16:16:32 -0700

    edit pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  # get 'sections/index'
+
+  # get 'sections/show'
+
+  # get 'sections/new'
+
+  # get 'sections/edit'
+
+  # get 'sections/delete'
+
+  # get 'pages/index'
+
+  # get 'pages/show'
+
+  # get 'pages/new'
+
+  # get 'pages/edit'
+
+  # get 'pages/delete'
+
 #   get 'subjects/index'
 
 #   get 'subjects/show'
