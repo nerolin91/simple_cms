65e837c31c08cfce8668588b75655cb97ff5191c
Nero Lin <nerolin91@gmail.com>
2016-10-21 21:54:06 -0700

    new controllers

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,17 @@
+class SubjectsController < ApplicationController
+  layout false
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+#   get 'subjects/index'
+
+#   get 'subjects/show'
+
+#   get 'subjects/new'
+
+#   get 'subjects/edit'
+
+#   get 'subjects/delete'
+
   match ':controller(/:action(/:id))', :via => :get
   root 'demo#index'
 
